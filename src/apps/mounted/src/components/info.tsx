cff710d249111a09ffae18667988d6d42c902179
copperdevs <github@copperdevs.com>
2025-12-10 21:59:44 -0600

    simple biome formatting

--- src/apps/mounted/src/components/info.tsx
+++ src/apps/mounted/src/components/info.tsx
@@ -2,18 +2,18 @@ import { useState } from "react";
 import type { JSX } from "react/jsx-runtime";
 
 export default function Info(): JSX.Element {
-  const [countValue, setCountValue] = useState<number>(0);
-  const increment = () => setCountValue(countValue + 1);
-  const decrement = () => setCountValue(countValue - 1);
+	const [countValue, setCountValue] = useState<number>(0);
+	const increment = () => setCountValue(countValue + 1);
+	const decrement = () => setCountValue(countValue - 1);
 
-  return (
-    <div>
-      <h2>info</h2>
-      <p>{document.location.origin}</p>
+	return (
+		<div>
+			<h2>info</h2>
+			<p>{document.location.origin}</p>
 
-      <h2>Count Value: {countValue}</h2>
-      <button onClick={decrement}>-1</button>
-      <button onClick={increment}>+1</button>
-    </div>
-  );
+			<h2>Count Value: {countValue}</h2>
+			<button onClick={decrement}>-1</button>
+			<button onClick={increment}>+1</button>
+		</div>
+	);
 }
